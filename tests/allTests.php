43ce4803fc591711cab63e3bbda987378c1eabc4
Lars Strojny <lars@strojny.net>
2007-07-01 22:56:22 +0000

    Break test suite on purpose

--- tests/allTests.php
+++ tests/allTests.php
@@ -19,7 +19,7 @@
  */
 
 error_reporting(E_ALL | E_STRICT);
-require_once 'PHPUnit/Framework/TestSuite.php';
+,require_once 'PHPUnit/Framework/TestSuite.php';
 require_once 'PHPUnit/TextUI/TestRunner.php';
 class allTests
 {
