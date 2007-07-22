a8ab954e0f36590da8cbdeb39ce06d2713fc2ae0
Mario Heiderich <mario.heiderich@gmail.com>
2007-07-22 21:17:17 +0000

    modified the rules and killed several false +ves from christians planet.

--- tests/IDS/MonitorTest.php
+++ tests/IDS/MonitorTest.php
@@ -101,7 +101,7 @@ class IDS_MonitorTest extends PHPUnit_Framework_TestCase {
         );
         $result = $test->run();
         $this->assertTrue($result->hasEvent(1));
-        $this->assertEquals(213, $result->getImpact());        
+        $this->assertEquals(193, $result->getImpact());        
     }
 
     public function testSQLIList() {
