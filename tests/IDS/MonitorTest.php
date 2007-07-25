e8b3871a30b4a7f9b11a8d27a5a0c0aeba7e8de9
Mario Heiderich <mario.heiderich@gmail.com>
2007-07-25 21:54:10 +0000

    modified the filter rules against sql injection vectors, customized the tests

--- tests/IDS/MonitorTest.php
+++ tests/IDS/MonitorTest.php
@@ -129,7 +129,7 @@ class IDS_MonitorTest extends PHPUnit_Framework_TestCase {
         );
         $result = $test->run();
         $this->assertTrue($result->hasEvent(1));
-        $this->assertEquals(53, $result->getImpact());        
+        $this->assertEquals(63, $result->getImpact());        
     }
     
     public function testDTList(){
