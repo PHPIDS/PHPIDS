def12284721ad46843b57066c2eb1be4f3202b40
Mario Heiderich <mario.heiderich@gmail.com>
2007-07-25 11:47:16 +0000

    modified the filter rules against false positives, fixed the tests

--- tests/IDS/MonitorTest.php
+++ tests/IDS/MonitorTest.php
@@ -110,7 +110,7 @@ class IDS_MonitorTest extends PHPUnit_Framework_TestCase {
         );
         $result = $test->run();
         $this->assertTrue($result->hasEvent(1));
-        $this->assertEquals(245, $result->getImpact());        
+        $this->assertEquals(220, $result->getImpact());        
     }
 
     public function testSQLIList() {
