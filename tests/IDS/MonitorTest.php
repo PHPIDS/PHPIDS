2971a2c24f3ab67e1951b6bd0c6eb8ee492efcb6
Mario Heiderich <mario.heiderich@gmail.com>
2007-08-06 08:37:28 +0000

    modified the filter rules to avoid false positives coming from user agents and special rewritten URLs

--- tests/IDS/MonitorTest.php
+++ tests/IDS/MonitorTest.php
@@ -173,7 +173,7 @@ class IDS_MonitorTest extends PHPUnit_Framework_TestCase {
         );
         $result = $test->run();
         $this->assertTrue($result->hasEvent(1));
-        $this->assertEquals(227, $result->getImpact());          
+        $this->assertEquals(222, $result->getImpact());          
     }    
     
     public function testRFEList() {
