10fb7a05fc912ac62b77a91aecabacfc755a4bcb
Mario Heiderich <mario.heiderich@gmail.com>
2007-08-09 23:21:48 +0000

    added detection for Gecko 1.9 threading dricetives, fixed the tests
    Example:
    background = Cc['@mozilla.org/thread-manager;1'].getService().newThread(0);
    main = Cc['@mozilla.org/thread-manager;1'].getService().mainThread;
    background.dispatch(new Foo(id), background.DISPATCH_NORMAL);

--- tests/IDS/MonitorTest.php
+++ tests/IDS/MonitorTest.php
@@ -247,7 +247,7 @@ class IDS_MonitorTest extends PHPUnit_Framework_TestCase {
         );
         $result = $test->run();
         $this->assertTrue($result->hasEvent(1));
-        $this->assertEquals(222, $result->getImpact());          
+        $this->assertEquals(227, $result->getImpact());          
     }    
     
     public function testRFEList() {
