c35d297bd18e83b05f7933121abe3f3e62526969
Mario Heiderich <mario.heiderich@gmail.com>
2007-09-15 16:51:01 +0000

    fixed the rules against xorrers latest intrusion, fixed the tests

--- tests/IDS/MonitorTest.php
+++ tests/IDS/MonitorTest.php
@@ -249,7 +249,7 @@ class IDS_MonitorTest extends PHPUnit_Framework_TestCase {
         $result = $test->run();
         $this->assertTrue($result->hasEvent(1));
         
-        $this->assertEquals(288, $result->getImpact());        
+        $this->assertEquals(292, $result->getImpact());        
     }     
     
     public function testXSSList() {
