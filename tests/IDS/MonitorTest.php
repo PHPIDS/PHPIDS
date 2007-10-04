7aabb62f8a3b9c5b03456a746b4e58ec60ea73cf
Mario Heiderich <mario.heiderich@gmail.com>
2007-10-04 16:08:22 +0000

    added better detection for JS function declaration and fixed the tests

--- tests/IDS/MonitorTest.php
+++ tests/IDS/MonitorTest.php
@@ -340,7 +340,7 @@ class IDS_MonitorTest extends PHPUnit_Framework_TestCase {
         $result = $test->run();
         $this->assertTrue($result->hasEvent(1));
         
-        $this->assertEquals(176, $result->getImpact());        
+        $this->assertEquals(181, $result->getImpact());        
     }    
 
     public function testXMLPredicateXSSList() {
