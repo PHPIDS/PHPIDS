b48d5565c2b8b10079c78dbfe3f092450d0b6500
Mario Heiderich <mario.heiderich@gmail.com>
2007-11-20 16:22:37 +0000

    fixed the tests

--- tests/IDS/MonitorTest.php
+++ tests/IDS/MonitorTest.php
@@ -751,7 +751,7 @@ class IDS_MonitorTest extends PHPUnit_Framework_TestCase {
         );
         $result = $test->run();
         $this->assertTrue($result->hasEvent(1));
-        $this->assertEquals(114, $result->getImpact());
+        $this->assertEquals(120, $result->getImpact());
     }
 
     public function testURIList(){
