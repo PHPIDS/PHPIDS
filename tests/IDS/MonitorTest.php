25b8c8cc92ad65016fcc5dc245e1f51110beaef5
Mario Heiderich <mario.heiderich@gmail.com>
2008-01-17 23:04:05 +0000

    fixed a false alert

--- tests/IDS/MonitorTest.php
+++ tests/IDS/MonitorTest.php
@@ -521,7 +521,7 @@ class IDS_MonitorTest extends PHPUnit_Framework_TestCase {
         );
         $result = $test->run();
         $this->assertTrue($result->hasEvent(1));
-        $this->assertEquals(445, $result->getImpact());
+        $this->assertEquals(440, $result->getImpact());
     }
 
     public function testSQLIList2() {
