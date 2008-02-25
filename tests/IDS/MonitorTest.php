a6ac81b699110f1666491c453b65ebd23eb57572
Mario Heiderich <mario.heiderich@gmail.com>
2008-02-25 16:45:12 +0000

    fixed a minor false positive found by aung khant

--- tests/IDS/MonitorTest.php
+++ tests/IDS/MonitorTest.php
@@ -899,7 +899,7 @@ class IDS_MonitorTest extends PHPUnit_Framework_TestCase {
         );
         $result = $test->run();
         $this->assertTrue($result->hasEvent(1));
-        $this->assertEquals(491, $result->getImpact());
+        $this->assertEquals(495, $result->getImpact());
     }
 
     public function testUTF7List() {
