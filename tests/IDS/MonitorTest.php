b4a92ef7c11cd9048a6d8cc6705132717d0e9295
Mario Heiderich <mario.heiderich@gmail.com>
2009-02-09 18:32:17 +0000

    * fixed the CI

--- tests/IDS/MonitorTest.php
+++ tests/IDS/MonitorTest.php
@@ -440,7 +440,7 @@ class IDS_MonitorTest extends PHPUnit_Framework_TestCase {
             $this->init
         );
         $result = $test->run();
-	$this->assertImpact($result, 837, 825);
+	$this->assertImpact($result, 837, 820);
     }
 
     public function testXMLPredicateXSSList() {
@@ -548,7 +548,7 @@ class IDS_MonitorTest extends PHPUnit_Framework_TestCase {
             $this->init
         );
         $result = $test->run();
-        $this->assertImpact($result, 510, 534);
+        $this->assertImpact($result, 510, 530);
     }
 
     public function testSelfContainedXSSList() {
@@ -987,7 +987,7 @@ class IDS_MonitorTest extends PHPUnit_Framework_TestCase {
             $this->init
         );
         $result = $test->run();
-        $this->assertImpact($result, 131, 141);
+        $this->assertImpact($result, 131, 136);
     }
 
     public function testRFEList() {
