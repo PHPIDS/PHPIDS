4cd266974568bbce9cc96ad4c5bfe79e562faa79
Mario Heiderich <mario.heiderich@gmail.com>
2010-07-02 21:17:40 +0000

    * post fixed the suhosin tests

--- tests/IDS/MonitorTest.php
+++ tests/IDS/MonitorTest.php
@@ -488,7 +488,7 @@ class IDS_MonitorTest extends PHPUnit_Framework_TestCase {
             $this->init
         );
         $result = $test->run();
-        $this->assertEquals(1032, $result->getImpact());
+        $this->assertImpact($result, 1032, 1019);
     }
 
     public function testXMLPredicateXSSList() {
