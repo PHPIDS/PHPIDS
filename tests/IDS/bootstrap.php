fbca095714ad94941b8c121f0b9d9ab2dda6b6f5
Lars Strojny <lars@strojny.net>
2012-10-03 00:16:22 +0200

    Create library cache dir as well in bootstrap file #2

--- tests/IDS/bootstrap.php
+++ tests/IDS/bootstrap.php
@@ -4,3 +4,9 @@ $cacheDir = __DIR__ . '/../../tmp';
 if (!file_exists($cacheDir)) {
     mkdir($cacheDir, 0700);
 }
+
+/** Create library cache dir */
+$libraryCacheDir = __DIR__ . '/../../lib/IDS/tmp';
+if (!file_exists($libraryCacheDir)) {
+    mkdir($libraryCacheDir, 0700);
+}
