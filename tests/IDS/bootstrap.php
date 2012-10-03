0fe5f32da90dcd205a2913a3f1ebc42c3d58ccbb
Lars Strojny <lars@strojny.net>
2012-10-03 11:55:37 +0200

    PHP 5.2 compatibility

--- tests/IDS/bootstrap.php
+++ tests/IDS/bootstrap.php
@@ -1,12 +1,12 @@
 <?php
 /** Create temporary directory */
-$cacheDir = __DIR__ . '/../../tmp';
+$cacheDir = dirname(__FILE__) . '/../../tmp';
 if (!file_exists($cacheDir)) {
     mkdir($cacheDir, 0700);
 }
 
 /** Create library cache dir */
-$libraryCacheDir = __DIR__ . '/../../lib/IDS/tmp';
+$libraryCacheDir = dirname(__FILE__) . '/../../lib/IDS/tmp';
 if (!file_exists($libraryCacheDir)) {
     mkdir($libraryCacheDir, 0700);
 }
