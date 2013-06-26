ef220beee03d5147fcfe37ed8cf6bdb0571039ca
Lars Strojny <lars@strojny.net>
2013-06-26 15:41:38 +0200

    trying to fix example

--- docs/examples/example.php
+++ docs/examples/example.php
@@ -16,7 +16,7 @@
  * GNU General Public License for more details.
  */
 
-require_once __DIR__.'/../../autoload.php';
+require_once __DIR__.'/../../vendor/autoload.php';
 
 use IDS\Init;
 use IDS\Monitor;
